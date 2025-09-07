4f8fac994ab171be9debf70d116a8db5ef6558fa
Whilmar Bitoco <whlmrbitoco@gmail.com>
2025-09-07 08:39:49 +0800

    fix deps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,10 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 
-import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
   solidity: {
     profiles: {
       default: {
